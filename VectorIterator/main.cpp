5b736ec484f0fadad96eb3f59dc34a71d43689a5
SofyaTorosyan <sofyatorosian@gmail.com>
2023-12-17 08:02:28 -0800

    VectorIterator

--- VectorIterator/main.cpp
+++ VectorIterator/main.cpp
@@ -11,26 +11,26 @@ int main()
     testCopyConstructor();
 
     vector<int> vec(4, 5);
-    //vector<int> vec1 = vec;
-    //vec1.push_back(2);
-    //vec1.push_back(2);
-    //vec1.push_back(2);
-    //std::cout << vec1[2] << std::endl;
-    //vec1.print();
-    //vector<int>::Iterator it = vec1.begin();
-    //vec1[1] = 100;
-    //vec1.print();
-    //std::cout << "*it: " << *it  << std::endl;
-    //std::cout << "*it: " << *(++it)  << std::endl;
-    //it = ++vec1.begin();
-    //std::cout << "*(--it): " << *(--it)  << std::endl;
-    //it = vec1.begin() + 2;
-    //std::cout << "*it: " << *it  << std::endl;
-    //
-    //vector<int> vec2 = {1,2,3};
-    //std::cout << *vec2.begin();
-    //std::cout << "empty ? " << vec2.empty() << std::endl;
-    //vec1 = vec1;
+    vector<int> vec1 = vec;
+    vec1.push_back(2);
+    vec1.push_back(2);
+    vec1.push_back(2);
+    std::cout << vec1[2] << std::endl;
+    vec1.print();
+    vector<int>::Iterator it = vec1.begin();
+    vec1[1] = 100;
+    vec1.print();
+    std::cout << "*it: " << *it  << std::endl;
+    std::cout << "*it: " << *(++it)  << std::endl;
+    it = ++vec1.begin();
+    std::cout << "*(--it): " << *(--it)  << std::endl;
+    it = vec1.begin() + 2;
+    std::cout << "*it: " << *it  << std::endl;
+    
+    vector<int> vec2 = {1,2,3};
+    std::cout << *vec2.begin();
+    std::cout << "empty ? " << vec2.empty() << std::endl;
+    vec1 = vec1;
     vector<int> vec2 = {1,2,3};
     vec.print();
     vec2.print();
